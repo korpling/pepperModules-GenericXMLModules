8080cf4a6ea4652d91e8b579682176d857926652
annis <korpling@hu-berlin.de>
2013-04-16 11:56:06 +0000

    [maven-release-plugin]  copy for tag pepperModules-GenericXMLModules-1.1.0
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/PepperModules/GenericXMLModules/tags/pepperModules-GenericXMLModules-1.1.0@7728 47085195-c50e-4317-8f43-7cb808db0f00

---
+++ trunk/src/main/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/genericXMLModules/GenericXMLImporterProperties.java
@@ -0,0 +1,337 @@
+/**
+ * Copyright 2009 Humboldt University of Berlin, INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+package de.hu_berlin.german.korpling.saltnpepper.pepperModules.genericXMLModules;
+
+import java.util.Collection;
+import java.util.Vector;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.PepperModuleProperties;
+import de.hu_berlin.german.korpling.saltnpepper.pepper.pepperModules.PepperModuleProperty;
+import de.hu_berlin.german.korpling.saltnpepper.pepperModules.genericXMLModules.xpath.XPathExpression;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sCorpusStructure.SDocument;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SSpan;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SStructure;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SStructuredNode;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.STextualDS;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SToken;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SAnnotation;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SLayer;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCore.SMetaAnnotation;
+
+/**
+ * Defines the properties to be used for the {@link GenericXMLImporter}. 
+ * @author Florian Zipser
+ *
+ */
+public class GenericXMLImporterProperties extends PepperModuleProperties 
+{
+	public static final String PREFIX="genericXml.importer.";
+	
+	/**
+	 * Name of property of the nodes to be ignored while import.
+	 */
+	public static final String PROP_IGNORE_LIST=PREFIX+"ignoreList";
+	/**
+	 * Name of property of element-nodes to map to an {@link SSpan} instead of an {@link SStructure}
+	 */
+	public static final String PROP_AS_SPANS=PREFIX+"asSSpan";
+	/**
+	 * Name of property containing a list of all element-nodes whos attributes shall be prefixed by the name of the element-node 
+	 */
+	public static final String PROP_PREFIXED_ANNOS=PREFIX+"prefixSAnnotationName";
+	/**
+	 * Name of property to determine if an artificial {@link SStructuredNode} shall be created for all text-nodes.
+	 */
+	public static final String PROP_ARTIFICIAL_SSTRUCT=PREFIX+"artificialSStruct";
+	/**
+	 * Name of property containing a list of element-nodes or attribute nodes to be mapped to {@link SMetaAnnotation} 
+	 * objects instead of {@link SAnnotation} objects. 
+	 */
+	public static final String PROP_SMETA_ANNOTATION=PREFIX+"sMetaAnnotation";
+	/**
+	 * Name of property to determine subtrees to be mapped to {@link SMetaAnnotation} to the entire {@link SDocument}
+	 */
+	public static final String PROP_SMETA_ANNOTATION_SDOCUMENT=PREFIX+"sMetaAnnotation.sDocument";
+	/**
+	 * Name of property determining if only text is imported.
+	 */
+	public static final String PROP_TEXT_ONLY=PREFIX+"textOnly";
+	/**
+	 * Name of property containing a list of element-nodes being mapped to {@link SLayer} objects.
+	 */
+	public static final String PROP_SLAYER=PREFIX+"sLayer";
+	/**
+     * Name of property to determine which whitespace characters are ignored and add to {@link STextualDS} object, 
+     * but do not get an own {@link SToken} object. A list separated by ',' for instance /n,/r, ,... for linefeed, 
+     * carriage return and blank.
+     * Default is {@value #DEFAULT_WHITESPACES}.
+     */
+    public static final String PROP_IGNORABLE_WHITESPACES =PREFIX+"ignoreWhitepsaces";
+	/**
+     * Name of property to determine if namespaces are ignored.
+     * Default is true.
+     */
+    public static final String PROP_IGNORABLE_NAMESPACES =PREFIX+"ignoreNamespaces";
+    /**
+     * Name of property to determine a list of element-nodes whichs names are mapped to {@link SAnnotation} objects having that
+     * name as sName and sValue
+     */
+    public static final String PROP_ELEMENTNAME_AS_SANNO =PREFIX+"elementNameAsSAnno";
+	/**
+	 * Name of property to determine the file ending of documents to be imported.
+	 */
+	public static final String PROP_FILE_ENDINGS=PREFIX+"file.endings";
+	
+	/**
+	 * When the list of file endings contain this string, all file endings will be accepted
+	 */
+	public static final String KW_ALL="ALL";
+	
+	/**
+	 * Default whitespaces to be ignored.
+	 */
+	public static final String DEFAULT_WHITESPACES= "'\n','\r','\t',' '";
+	
+	public GenericXMLImporterProperties()
+	{
+		this.addProperty(new PepperModuleProperty<String>(PROP_IGNORE_LIST, String.class, "IgnoreList is a list of nodes (element nodes , attribute -nodes and text-nodes, which shall be ignored while processing. Note that if an element node is part of the ignore list, its subtree will also be ignored)", false));
+		this.addProperty(new PepperModuleProperty<String>(PROP_AS_SPANS, String.class, "In case of you don't want to map an element node to an SStructure, you can map it to an SSpan. Note that this is only possible, if the element node directly contains a text node.", false));
+		this.addProperty(new PepperModuleProperty<String>(PROP_PREFIXED_ANNOS, String.class, "You can set this flag to prefix the SName of the SAnnotation with the name of the surrounding element node.", false));
+		this.addProperty(new PepperModuleProperty<String>(PROP_SMETA_ANNOTATION, String.class, "Determines a list a list of attribute names mapped to SMetaAnnotation objects instead of SAnnotationobjects.", false));
+		this.addProperty(new PepperModuleProperty<String>(PROP_SMETA_ANNOTATION_SDOCUMENT, String.class, "Determines a list of element-nodes which subtrees are mapped to meta-data on entire SDocument.", false));
+		this.addProperty(new PepperModuleProperty<String>(PROP_SLAYER, String.class, "Determines a list of element-nodes which are mapped to a SLayer object.", false));
+		this.addProperty(new PepperModuleProperty<Boolean>(PROP_ARTIFICIAL_SSTRUCT, Boolean.class, "If set to true, for each text node an artificial SStructuredNode will be created and overlap the also created (always even if this property is set to false) SToken node.", false, false));
+		this.addProperty(new PepperModuleProperty<Boolean>(PROP_TEXT_ONLY, Boolean.class, "Determines if only text-nodes are mapped.", false, false));
+		this.addProperty(new PepperModuleProperty<String>(   PROP_IGNORABLE_WHITESPACES, 
+		                                                      String.class, 
+		                                                      "Determine which whitespace characters are ignored and add to {@link STextualDS} object,     * but do not get an own {@link SToken} object. A list separated by ',' for instance \\n,\\r, ,... for linefeed, carriage return and blank.", 
+		                                                      DEFAULT_WHITESPACES, 
+		                                                      false));
+		this.addProperty(new PepperModuleProperty<Boolean>(PROP_IGNORABLE_NAMESPACES, Boolean.class, "Determines if xml-namespaces and namespace declarations are ignored.", true, false));
+		this.addProperty(new PepperModuleProperty<String>(PROP_ELEMENTNAME_AS_SANNO, String.class, " Determines a list of element-nodes whichs names are mapped to SAnnotation objects having that name as sName and sValue", false));
+		this.addProperty(new PepperModuleProperty<String>(PROP_FILE_ENDINGS, String.class, "Determines a list, containing the file endings, which files shall be imported. If you want to import all contained files no matter to their ending, add the string 'ALL' to the list. ", "ALL", false));
+	}
+	
+	public EList<String> getFileEndings()
+	{
+		EList<String> retVal= new BasicEList<String>();
+		String endingList= (String)this.getProperty(PROP_FILE_ENDINGS).getValue();
+		String[] endings= endingList.split(",");
+		if (endings!= null)
+		{
+			for (String ending: endings)
+				retVal.add(ending.trim());
+		}
+		return(retVal);
+	}
+	
+	/**
+	 * Set of {@link XPathExpression}, which nodes shall be ignored including their subtrees in case of they are element-nodes
+	 */
+	private Collection<XPathExpression> ignoreList= null;
+	/**
+	 * Returns set of {@link XPathExpression}, which nodes shall be ignored including their subtrees in case of they are element-nodes
+	 * @return
+	 */
+	public Collection<XPathExpression> getIgnoreList() {
+		if (ignoreList== null)
+		{
+			Collection<XPathExpression> xPathList= extractXPathExpr(PROP_IGNORE_LIST); 
+			if (ignoreList== null)
+				ignoreList= xPathList;
+		}
+		return ignoreList;
+	}
+	
+	/**
+	 * set of {@link XPathExpression}, which nodes shall be mapped to {@link SSpan} objects instead of {@link SStructure} objects.
+	 */
+	private Collection<XPathExpression> asSpans= null;
+	/**
+	 * Returns set of {@link XPathExpression}, which nodes shall be mapped to {@link SSpan} objects instead of {@link SStructure} objects.
+	 * @return
+	 */
+	public Collection<XPathExpression> getAsSpans() {
+		if (asSpans== null)
+		{
+			Collection<XPathExpression> xPathList= extractXPathExpr(PROP_AS_SPANS); 
+			if (asSpans== null)
+				asSpans= xPathList;
+		}
+		return asSpans;
+	}
+	
+	/**
+	 * set of {@link XPathExpression}, which attribute nodes shall be prefixed with the element-node name.
+	 */
+	private Collection<XPathExpression> prefixedAnnoList= null;
+	/**
+	 * Returns set of {@link XPathExpression}, which attribute nodes shall be prefixed with the element-node name.
+	 * @return
+	 */
+	public Collection<XPathExpression> getPrefixedAnnoList() {
+		if (prefixedAnnoList== null)
+		{
+			Collection<XPathExpression> xPathList= extractXPathExpr(PROP_PREFIXED_ANNOS); 
+			if (prefixedAnnoList== null)
+				prefixedAnnoList= xPathList;
+		}
+		return prefixedAnnoList;
+	}
+	/**
+	 * set of {@link XPathExpression}, which attribute nodes shall be mapped to {@link SMetaAnnotation} objects
+	 */
+	private Collection<XPathExpression> sMetaAnnoList= null;
+	/**
+	 * Returns set of {@link XPathExpression}, which attribute nodes shall be mapped to {@link SMetaAnnotation} objects.
+	 * @return
+	 */
+	public Collection<XPathExpression> getSMetaAnnotationList() {
+		if (sMetaAnnoList== null)
+		{
+			Collection<XPathExpression> xPathList= extractXPathExpr(PROP_SMETA_ANNOTATION); 
+			if (sMetaAnnoList== null)
+				sMetaAnnoList= xPathList;
+		}
+		return sMetaAnnoList;
+	}
+	/**
+	 * set of {@link XPathExpression}, which attribute nodes shall be mapped to {@link SMetaAnnotation} objects on the {@link SDocument}
+	 */
+	private Collection<XPathExpression> sMetaAnnoSDocumentList= null;
+	/**
+	 * Returns set of {@link XPathExpression}, which attribute nodes shall be mapped to {@link SMetaAnnotation} objects on the {@link SDocument}.
+	 * @return
+	 */
+	public Collection<XPathExpression> getSMetaAnnotationSDocumentList() {
+		if (sMetaAnnoSDocumentList== null)
+		{
+			Collection<XPathExpression> xPathList= extractXPathExpr(PROP_SMETA_ANNOTATION_SDOCUMENT); 
+			if (sMetaAnnoSDocumentList== null)
+				sMetaAnnoSDocumentList= xPathList;
+		}
+		return sMetaAnnoSDocumentList;
+	}
+	/**
+	 * set of {@link XPathExpression}, which element-nodes shall be mapped to {@link SLayer} objects.
+	 */
+	private Collection<XPathExpression> sLayerList= null;
+	/**
+	 * Returns set of {@link XPathExpression}, which element-nodes shall be mapped to {@link SLayer} objects.
+	 * @return
+	 */
+	public Collection<XPathExpression> getSLayerList() {
+		if (sLayerList== null)
+		{
+			Collection<XPathExpression> xPathList= extractXPathExpr(PROP_SLAYER); 
+			if (sLayerList== null)
+				sLayerList= xPathList;
+		}
+		return sLayerList;
+	}
+	private synchronized Collection<XPathExpression> extractXPathExpr(String propName)
+	{
+		Collection<XPathExpression> xPathList= new Vector<XPathExpression>();
+		PepperModuleProperty<?> prop= this.getProperty(propName); 
+		if (prop!= null)
+		{	
+			if (prop.getValue()!= null)
+			{
+				String[] xPathListStr= ((String)prop.getValue()).split(",");
+				if (xPathList!= null)
+				{
+					for (String xPathString : xPathListStr)
+						xPathList.add(new XPathExpression(xPathString.trim())); 
+				}
+			}
+		}
+		return(xPathList);
+	}
+	
+	/**
+	 * Returns true if artificial {@link SStructuredNode} object shall be created.
+	 * @return
+	 */
+	public boolean isCreateSStructure()
+	{
+		return((Boolean)this.getProperty(PROP_ARTIFICIAL_SSTRUCT).getValue());
+	}
+	/**
+	 * Returns if only text shall be mapped.
+	 * @return
+	 */
+	public boolean isTextOnly()
+	{
+		return((Boolean)this.getProperty(PROP_TEXT_ONLY).getValue());
+	}
+	/**
+	 * Returns if xml-namespaces and namespace declarations are ignored..
+	 * @return
+	 */
+	public boolean isIgnoreNamespaces()
+	{
+		return((Boolean)this.getProperty(PROP_IGNORABLE_NAMESPACES).getValue());
+	}
+	/**
+	 * Determine which whitespace characters are ignored and add to {@link STextualDS} object, 
+     * but do not get an own {@link SToken} object. A list separated by ',' for instance /n,/r, ,... for linefeed, 
+     * carriage return and blank.
+	 */
+	private Collection<String> ignorableWhitespaces= null;
+	/**
+     * determine which whitespace characters are ignored and add to {@link STextualDS} object, 
+     * but do not get an own {@link SToken} object. A list separated by ',' for instance /n,/r, ,... for linefeed, 
+     * carriage return and blank.
+     */
+    public Collection<String> getIgnorableWhitespaces() {
+        if (ignorableWhitespaces== null)
+        {
+            PepperModuleProperty<String> prop= (PepperModuleProperty<String>) this.getProperty(PROP_IGNORABLE_WHITESPACES);
+            if (prop.getValue()!= null)
+            {
+                ignorableWhitespaces= new Vector<String>();
+                String[] parts= prop.getValue().split(",");
+                for (String part: parts)
+                {
+                    ignorableWhitespaces.add(part.replace("'", ""));
+                }
+            }
+        }
+        return ignorableWhitespaces;
+    }
+	/**
+	 * see {@link #PROP_ELEMENTNAME_AS_SANNO}
+	 */
+	private Collection<XPathExpression> elementNameAsSAnnoList= null;
+	/**
+	 * see {@link #PROP_ELEMENTNAME_AS_SANNO}
+	 * @return
+	 */
+	public Collection<XPathExpression> getElementNameAsSAnnoList() {
+		if (elementNameAsSAnnoList== null)
+		{
+			Collection<XPathExpression> xPathList= extractXPathExpr(PROP_ELEMENTNAME_AS_SANNO); 
+			if (elementNameAsSAnnoList== null)
+				elementNameAsSAnnoList= xPathList;
+		}
+		return elementNameAsSAnnoList;
+	}
+}
---
+++ trunk/src/test/java/de/hu_berlin/german/korpling/saltnpepper/pepperModules/genericXMLModules/xpath/tests/XPathExpressionTest.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright 2009 Humboldt University of Berlin, INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+package de.hu_berlin.german.korpling.saltnpepper.pepperModules.genericXMLModules.xpath.tests;
+import java.util.List;
+import java.util.Vector;
+
+import de.hu_berlin.german.korpling.saltnpepper.pepperModules.genericXMLModules.xpath.XPathExpression;
+
+import junit.framework.TestCase;
+
+
+public class XPathExpressionTest extends TestCase {
+
+	private XPathExpression fixture= null;
+	public void setFixture(XPathExpression fixture) {
+		this.fixture = fixture;
+	}
+
+	public XPathExpression getFixture() {
+		return fixture;
+	}
+	public void setUp()
+	{
+		this.setFixture(new XPathExpression());
+	}
+
+	/**
+	 * test element-node
+	 */
+	public void testElementNode()
+	{
+		assertTrue(XPathExpression.matches(new XPathExpression("/element1/element2/element3"), new XPathExpression("/element1/element2/element3")));
+		assertFalse(XPathExpression.matches(new XPathExpression("/element1/element2/element4"), new XPathExpression("/element1/element2/element3")));
+		assertFalse(XPathExpression.matches(new XPathExpression("/element1/element4/element3"), new XPathExpression("/element1/element2/element3")));
+	}
+	
+	/**
+	 * test attribute-node 
+	 */
+	public void testAttributeNode()
+	{
+		assertTrue(XPathExpression.matches(new XPathExpression("/element1/element2/@attribute1"), new XPathExpression("/element1/element2/@attribute1")));
+		assertFalse(XPathExpression.matches(new XPathExpression("/element1/element2/@attribute2"), new XPathExpression("/element1/element2/@attribute1")));
+	}
+	
+	/**
+	 * test text-node 
+	 */
+	public void testTextNode()
+	{
+		assertTrue(XPathExpression.matches(new XPathExpression("/element1/element2/test()"), new XPathExpression("/element1/element2/test()")));
+		assertFalse(XPathExpression.matches(new XPathExpression("/element1/element3/test()"), new XPathExpression("/element1/element2/@attribute1")));
+	}
+	
+	/**
+	 * test element-node wildcard
+	 */
+	public void testElementNode_Wildcards()
+	{
+		assertTrue(XPathExpression.matches(new XPathExpression("//"), new XPathExpression("/element1/element2/element3")));
+		assertTrue(XPathExpression.matches(new XPathExpression("//element3"), new XPathExpression("/element1/element2/element3")));
+		assertTrue(XPathExpression.matches(new XPathExpression("element1//"), new XPathExpression("/element1/element2/element3")));
+		assertFalse(XPathExpression.matches(new XPathExpression("//element1"), new XPathExpression("/element1/element2/element3")));
+		assertFalse(XPathExpression.matches(new XPathExpression("//element2"), new XPathExpression("/element1/element2/element3")));
+		assertTrue(XPathExpression.matches(new XPathExpression("//element2//"), new XPathExpression("/element1/element2/element3")));
+		assertTrue(XPathExpression.matches(new XPathExpression("//element2//element4"), new XPathExpression("/element1/element2/element3/element4")));
+	}
+	
+	/**
+	 * test element-node wildcard and checks if it also works with using {@link XPathExpression#addStep(String)}
+	 */
+	public void testElementNode_Wildcards2()
+	{		
+		XPathExpression xpr1= new XPathExpression();
+		xpr1.addStep("//");
+		xpr1.addStep("head");
+		xpr1.addStep("//");
+		
+		XPathExpression xpr2= new XPathExpression();
+		xpr2.addStep("text");
+		xpr2.addStep("body");
+		xpr2.addStep("div");
+		xpr2.addStep("head");
+		xpr2.addStep("foreign");
+		
+		assertTrue("xpr '"+xpr1+"' shall match '"+xpr2+"'", XPathExpression.matches(xpr1, xpr2));
+		
+		xpr2.removeLastStep();
+		xpr2.removeLastStep();
+		xpr2.addStep("p");
+		
+		assertFalse("xpr '"+xpr1+"' shall not match '"+xpr2+"'", XPathExpression.matches(xpr1, new XPathExpression("/text/body/div/p")));
+	}
+	
+	/**
+	 * test attribute-node wildcard
+	 */
+	public void testAttributeNode_Wildcard()
+	{
+		assertTrue(XPathExpression.matches(new XPathExpression("//@attribute1"), new XPathExpression("/element1/element2/@attribute1")));
+	}
+	
+	/**
+	 * test text-node wildcard
+	 */
+	public void testTextNode_Wildcard()
+	{
+		assertTrue(XPathExpression.matches(new XPathExpression("//text()"), new XPathExpression("/element1/element2/text()")));
+	}
+}
